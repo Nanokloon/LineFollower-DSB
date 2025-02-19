b9f4a43bdbc46c26d3ff3b8c6e71bcb784717b21
Nanokloon <mathzone1@outlook.com>
2025-02-19 19:25:47 +0100

    pain and stuffering

--- entities/motorcontrol.sv
+++ entities/motorcontrol.sv
@@ -1,10 +1,52 @@
+
 module motorcontrol 
    (input logic clk,
     input logic reset,
     input logic direction, 
-    input logic [?:0] count_in,
+    input logic [16:0] count_in,
     output logic pwm);
     
+    typedef enum logic { motor_off, motor_cw, motor_ccw } motor_controller_state;
+    motor_controller_state state,next_state;
+
+    always_ff @(posedge clk)
+        if (reset)
+            state <= motor_off;
+        else
+            state <= next_state;
+    
+    always_comb //state logic
+    begin
+        case(state)
+            motor_off:
+            begin
+                if(direction ==? 0)
+                    next_state = motor_ccw;
+                else
+                    next_state = motor_cw;
+            end
+            motor_ccw:
+            begin
+                if(direction ==? 0)
+                    next_state = motor_ccw;
+                else
+                    next_state = motor_cw;
+            end
+            motor_cw:
+            begin
+                if(direction ==? 0)
+                    next_state = motor_ccw;
+                else
+                    next_state = motor_cw;
+            end
+        endcase
+    end
+
+    always_ff @(count_in)
+    begin
+    
+    
+    end
 
 
 endmodule
--- entities/timebase.sv
+++ entities/timebase.sv
@@ -1,8 +1,21 @@
 module timebase 
    (input logic clk,
     input logic reset,
-    output logic [?:0] count);
+    output logic [20:0] count);
+    logic [20:0] next_count;
+    always_ff @(posedge clk)
+    begin
+        if(reset)
+            count <= 0;
+        else
+            count <= next_count;
+    end
 
-
+    always_comb begin
+    if(count==?2000000)
+        next_count=0;
+    else
+        next_count=count+1;
+    end
 
 endmodule
