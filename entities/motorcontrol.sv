53a8ffd372f86fbcce5258684a456b3c191d566e
Nanokloon <mathzone1@outlook.com>
2025-02-19 20:09:22 +0100

    Deliverable 1 done!

--- entities/motorcontrol.sv
+++ entities/motorcontrol.sv
@@ -3,10 +3,10 @@ module motorcontrol
    (input logic clk,
     input logic reset,
     input logic direction, 
-    input logic [16:0] count_in,
+    input logic [20:0] count_in,
     output logic pwm);
     
-    typedef enum logic { motor_off, motor_cw, motor_ccw } motor_controller_state;
+    typedef enum logic [1:0] { motor_off, motor_cw, motor_ccw } motor_controller_state;
     motor_controller_state state,next_state;
 
     always_ff @(posedge clk)
@@ -37,16 +37,32 @@ module motorcontrol
                 if(direction ==? 0)
                     next_state = motor_ccw;
                 else
-                    next_state = motor_cw;
+                    next_state = motor_cw; 
             end
         endcase
     end
 
-    always_ff @(count_in)
+    always_comb
     begin
-    
+        case(state)
+            motor_off:
+                pwm=0;
+            motor_ccw:
+            begin
+                if(count_in <= 100000)
+                    pwm=1;
+                else
+                    pwm=0;
+            end
+            motor_cw:
+            begin
+                if(count_in <= 200000 )
+                    pwm=1;
+                else
+                    pwm=0;
+            end
+        endcase
     
     end
-
 
 endmodule
